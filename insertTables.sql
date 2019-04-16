30e9bfc2b010c470281a01ec941f53f9f410c219
pranavrd <34604452+pranavrd@users.noreply.github.com>
2019-04-16 22:09:44 +0530

    insert values commit #11

---
+++ insertTables.sql
@@ -0,0 +1,28 @@
+insert into movies values(1,'IP MAN','CHINESE',17:21:38,8.3,9);
+insert into movies values(2,'ANDHADHOON','HINDI',14:01:38,9.7,1);
+insert into movies values(3,'PARADISE NOW','ARABIC',23:48:06,5.6,5);
+insert into movies values(4,'THE INTOUCHBLES ','FRENCH',09:33:43,8.5,8);
+insert into movies values(5,'3 IDIOTS','HINDI',16:35:35,9.0,1);
+insert into movies values(6,'THE DARK KNIGHT','ENGLISH',14:35:19,9.4,2);
+insert into movies values(7,'LEVIATHAN','RUSSIAN',11:39:26,2.6,4);
+insert into movies values(8,'SOLARIS','PORTUGESE',06:36:25,6.5,3);
+insert into movies values(9,'DON'T WORRY,I'M FINE','SPANISH',05:58:34,0.5,0);
+insert into movies values(10,'OMAR','ARABIC',03:06:56,6.0,5);
+insert into movies values(11,'SHOLAY','HINDI',00:42:11,7.5,1);
+insert into movies values(12,'DON'T WORRY,I'M FINE,'PORTUGESE',01:10:12,0.2,3);
+insert into movies values(13,'Au Revoir les Enfants','FRENCH',04:21:38,1.6,8);
+insert into movies values(14,'INCEDIAS','FRENCH',21:44:44,4.6,8);
+insert into movies values(15,'THE ORPHANAGE','SPANISH',19:01:28,1.3,0);
+
+insert into country values(2,'USA');
+insert into country values(0,'SPAIN');
+insert into country values(9,'CHINA');
+insert into country values(1,'INDIA');
+insert into country values(4,'RUSSIA');
+insert into country values(8,'FRANCE');
+insert into country values(3,'PORTUGAL');
+insert into country values(5,'UAE');
+insert into country values(6,'ITALY');
+insert into country values(7,'GERMANY');
+
+insert into members values()
